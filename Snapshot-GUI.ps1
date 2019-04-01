dd9bae057f9244ec56df27772970da3e286ecaec
Sawyer Peacock <speacock@shorelinescasinos.com>
2019-04-01 16:27:43 -0400

    PowerCLI - Split GUI & Script
    Split the one file into two separated so it can be used generically if needed

---
+++ Snapshot-GUI.ps1
@@ -0,0 +1,76 @@
+<#
+.SYNOPSIS
+A GUI for Snapshot-Management.ps1
+
+.DESCRIPTION
+Allows for quick selection of regular vmware maintenance tasks for OGELP
+
+.EXAMPLE
+./Snapshot-GUI.ps1
+
+.NOTES
+AUTHORS
+Sawyer Peacock - OGELP SysAdmin
+
+FIXES
+Version 1.0
+Initial Commit
+
+Version 2.0
+Split GUI and functions
+Fixed some spelling mistakes
+#>
+############### Functions ############### 
+function PromptEnviroment() {
+    $Title = "GMS Environment"
+    $Info = "Please select the environment to manage snapshots"
+    $options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Production", "&Lab", "&Quit")
+    [int]$defaultchoice = 2
+    $opt = $host.UI.PromptForChoice($Title , $Info , $Options,$defaultchoice)
+    switch($opt)
+    {
+        0 { 
+        
+            $Script:ServerName = "ONTORVMM02.BLACKJACK.GCGAMING.COM"
+            $Script:VMListFile = "ServersProd.txt"
+            }
+        1 { 
+            $Script:ServerName = "ONBRMVMM01.blackjack.gcgaming.com"
+            $Script:VMListFile = "ServersLab.txt"
+            }
+        2 { 
+            Write-Host "Good Bye!!!" -ForegroundColor Green 
+            Return
+        }
+    }
+}
+
+function PromptAction() {
+    # Select What To Do
+    $Title = "Snapshot Action"
+    $Info = "How would you like to process snapshots"
+    $options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Create", "&Delete", "&Quit")
+    [int]$defaultchoice = 2
+    $Script:VMAction = $host.UI.PromptForChoice($Title , $Info , $Options,$defaultchoice)
+}
+
+############### Main Program ############### 
+
+$ServerName = $null
+$VMListFile = $null
+$VMAction = $null
+
+PromptEnviroment
+PromptAction
+
+#TODO: Check if any of the variables are null and quit
+
+switch($VMAction)
+{
+    0 {
+        ./Snapshot-Management -ServerName $ServerName -VMListFile $VMListFile -Create
+    }
+    1 {
+        ./Snapshot-Management -ServerName $ServerName -VMListFile $VMListFile -Delete
+    }
+}
--- Snapshot-Management.ps1
+++ Snapshot-Management.ps1
@@ -5,7 +5,24 @@ Automate the creation and removal of snapshots in a VMWare environment
 .DESCRIPTION
 This script will allow the user to loop through multiple VMs on a server and create or delete snapshots; primarily used during server maintenance routines
 
+.PARAMETER ServerName
+Specifies the ESXi host or vSphere server to connect to
+
+.PARAMETER VMListFile
+Specifies the txt file with the list of VM names (1 per line) that we would like to manage snapshots for
+
+.PARAMETER Create
+Tells the script that you want to create a snapshot for the VMs
+
+.PARAMETER Delete
+Tells the script that you want to DELETE the VM snapshots.
+NOTE: ALL VM shapshots are deleted; not just the one created by the script
+
 .EXAMPLE
+./Snapshot-Management.ps1 -ServerName vmhost.example.com -VMListFile servernames.txt -Create
+
+.EXAMPLE
+./Snapshot-Management.ps1 -ServerName vmhost.example.com -VMListFile servernames.txt -Delete
 
 .NOTES
 AUTHORS
@@ -14,48 +31,24 @@ Sawyer Peacock - OGELP SysAdmin
 FIXES
 Version 1.0
 Initial Commit
+
+Version 2.0
+Split GUI and functions
 #>
+param (
+	[string]$ServerName = $null,
+	[string]$VMListFile = $null,
+    [switch]$Create = $false,
+    [switch]$Delete = $false
+)
+
 Import-Module -Name "VMware.VimAutomation.Core"
 
 ############### Functions ############### 
-function PromptEnviroment() {
-    $Title = "GMS Environment"
-    $Info = "Please select the environment to manage snapshots"
-    $options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Production", "&Lab", "&Quit")
-    [int]$defaultchoice = 2
-    $opt = $host.UI.PromptForChoice($Title , $Info , $Options,$defaultchoice)
-    switch($opt)
-    {
-        0 { 
-        
-            $Script:ServerName = "ONTORVMM02.BLACKJACK.GCGAMING.COM"
-            $Script:VMListFile = "ServersProd.txt"
-            }
-        1 { 
-            $Script:ServerName = "ONBRMVMM01.blackjack.gcgaming.com"
-            $Script:VMListFile = "ServersLab.txt"
-            }
-        2 { 
-            Write-Host "Good Bye!!!" -ForegroundColor Green 
-            Return
-        }
-    }
-}
-
-function PromptAction() {
-    # Select What To Do
-    $Title = "Snapshot Action"
-    $Info = "How would you like to process snapshots"
-    $options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Create", "&Delete", "&Quit")
-    [int]$defaultchoice = 2
-    $Script:VMAction = $host.UI.PromptForChoice($Title , $Info , $Options,$defaultchoice)
-}
-
 function SnapShotCreate(){
     foreach($vm in Get-VM $VMlist) {
         Write-Host "Creating Snapshot for $($vm.name)"
         New-Snapshot -VM $vm -Memory:$false -Name "$($vm.Name)_BEFOREPATCH" -description "$($vmserver.User) Patching"
-	    # Start-Sleep -s 5
     }
 }
 
@@ -64,29 +57,15 @@ function SnapShotDelete() {
         Write-Host "Deleting Snapshot for $($vm.name)"
         Get-Snapshot -VM $vm |
         Remove-Snapshot -RemoveChildren:$true -Confirm:$false
-        
-	    # Start-Sleep -s 5
     }
 }
 
 ############### Main Program ############### 
 
-$ServerName = $null
-$VMListFile = $null
-$VMAction = $null
-
-PromptEnviroment
-PromptAction
-
 $VMlist = Get-Content -Path:$VMListFile
 $vmserver = Connect-VIServer -Server $ServerName -Protocol https
-switch($VMAction)
-{
-    0 {
-        SnapShotCreate
-    }
-    1 {
-        SnapShotDelete
-    }
-}
+
+if ($Delete) { SnapShotDelete }
+if ($Create) { SnapShotCreate }
+
 Disconnect-VIServer -Confirm:$false
